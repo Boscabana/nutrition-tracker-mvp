b1d2a99ba681ef21dcf227b63175e005fb81e866
Boscabana <82522050+Boscabana@users.noreply.github.com>
2026-08-25 11:26:57 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "9.3.1" apply false
+    id("com.android.application") version "9.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     // REMOVE THIS LINE: id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "2.3.2" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
 }
